e21311907846a45777f4d8e309ef45110dd70165
Brandon Vinh Le <brandonvinhquangle@gmail.com>
2022-02-21 18:21:46 -0700

    Working on airline names
    Co-authored-by: Joshua Higgins <jwhiggs77@gmail.com>
    Co-authored-by: taylorlesueur <taylorlesueur@users.noreply.github.com>

--- js/flights.js
+++ js/flights.js
@@ -1,54 +1,3 @@
-// /* Event Listener for the Submit Form */
-// document.getElementById("submit").addEventListener("click", validateForm);
-
-// /* Function that Validates that the Form is Filled Out Correctly */
-// function validateForm() {
-//   let origin = document.forms["myForm"]["origin"].value;
-//   let destination = document.forms["myForm"]["destination"].value;
-//   let currency = document.forms["myForm"]["currency"].value;
-  
-//   if (origin === "") {
-//     alert("Origin must be filled out");
-//     return false;
-//   }
-//   else if (destination === "") {
-//     alert("Destination must be filled out");
-//     return false;
-//   }
-//   else if (origin === destination) {
-//     alert("Origin must be different than destination")
-//     return false;
-//   }
-
-//   getCheapestFlight(origin, destination, currency);
-// }
-
-// /* Function that Fetches the Cheapest Flight Using an External API */
-// function getCheapestFlight(origin, destination, currency) {
-//   fetch("https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/v1/prices/cheap?origin=" + origin + "&page=None&currency=" + currency + "&destination=" + destination, {
-//     "method": "GET",
-//     "headers": {
-//       "x-access-token": "2ffd501228722984feea7ceb9cf73388",
-//       "x-rapidapi-host": "travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com",
-//       "x-rapidapi-key": "d6f870f92fmsh06e8bba4585eef3p12aa21jsn04f58e145da2"
-//     }
-//   }).then(function(response) {
-//     return response.json();
-//   }).then(function(json) {
-//     console.log(json);
-    
-//     /* Grab the Flight Data and Return to the Web Page */
-//     let results = "";
-//     results += '<h2>Flight info from ' + origin +  "to " + destination + "</h2>";
-//     results += "<p>";
-//     results += "TEST TEST TEST TEST TEST";
-//     results += "</p>";
-//     document.getElementById("flight-data").innerHTML = results;
-//   }).catch(err => {s
-//     console.error(err);
-//   });
-// }
-
 document.getElementById("submit").addEventListener("click", function(event) {
   event.preventDefault();
   const origin = document.getElementById("origin").value;
@@ -82,16 +31,48 @@ document.getElementById("submit").addEventListener("click", function(event) {
     console.log(json);
 
     let results = "";
-    results += "<h2>Flight info from " + origin +  " to " + destination + "</h2>";
-    results += "<p>";
-    for (let i = 0; json.data[destination].length; i++) {
-      results += json.data[destination][i];
-      console.log(json.data[destination][i]);
-    }
+    results += "<h2>Flight Details from " + origin +  " to " + destination + "</h2>";
+    let dest = json.data[destination];
+
+    for (let i = 0; i < Object.keys(dest).length; i++) {
+      let card = "<div class=\"card\"><div class=\"card-body\">";
     
-    results += "TEST TEST TEST TEST TEST";
-    results += "</p>";
+      // Airline
+      let code = dest[i].airline;
+      let airline = getAirlineName(code).then(response => console.log(response));
+      card += "<h3 class=\"airline-title\">Airline: " + airline + "</h3>"
+      
+      // Price
+      card += "<p class=\"price\">Ticket Price: $" + dest[i].price + ".00</p>";
+
+      // Flight Number
+      card += "<p class=\"flight-number\">Flight Number: #" + dest[i].flight_number + "</p>";
+
+      // Departure Date
+      card += "<p class=\"departure-date\">Departure Date: " + dest[i].departure_at + "</p>";
+
+      // Return Date
+      card += "<p class=\"return-date\">Return Date: " + dest[i].return_at + "</p>";
+      
+      card += "</div></div>";
+      results += card;
+    }
     console.log(results);
     document.getElementById("flight-data").innerHTML = results;
   });
 });
+s
+
+function getAirlineName(code) {
+  return fetch("https://iata-and-icao-codes.p.rapidapi.com/airline?iata_code=" + code, {
+        "method": "GET",
+        "headers": {
+          "x-rapidapi-host": "iata-and-icao-codes.p.rapidapi.com",
+          "x-rapidapi-key": "d6f870f92fmsh06e8bba4585eef3p12aa21jsn04f58e145da2"
+        }
+      }).then((response) => response.json())
+      .then(json => {
+        let airline = json[0].name;
+        return airline;
+      })
+}
