012f4038397a30c805279a579c1e639b273ef94c
Brandon Vinh Le <brandonvinhquangle@gmail.com>
2022-02-21 17:38:35 -0700

    Almost done with this dumb app

--- js/flights.js
+++ js/flights.js
@@ -1,12 +1,60 @@
-/* Event Listener for the Submit Form */
-document.getElementById("submit").addEventListener("click", validateForm);
+// /* Event Listener for the Submit Form */
+// document.getElementById("submit").addEventListener("click", validateForm);
 
-/* Function that Validates that the Form is Filled Out Correctly */
-function validateForm() {
-  let origin = document.forms["myForm"]["origin"].value;
-  let destination = document.forms["myForm"]["destination"].value;
-  let currency = document.forms["myForm"]["currency"].value;
+// /* Function that Validates that the Form is Filled Out Correctly */
+// function validateForm() {
+//   let origin = document.forms["myForm"]["origin"].value;
+//   let destination = document.forms["myForm"]["destination"].value;
+//   let currency = document.forms["myForm"]["currency"].value;
   
+//   if (origin === "") {
+//     alert("Origin must be filled out");
+//     return false;
+//   }
+//   else if (destination === "") {
+//     alert("Destination must be filled out");
+//     return false;
+//   }
+//   else if (origin === destination) {
+//     alert("Origin must be different than destination")
+//     return false;
+//   }
+
+//   getCheapestFlight(origin, destination, currency);
+// }
+
+// /* Function that Fetches the Cheapest Flight Using an External API */
+// function getCheapestFlight(origin, destination, currency) {
+//   fetch("https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/v1/prices/cheap?origin=" + origin + "&page=None&currency=" + currency + "&destination=" + destination, {
+//     "method": "GET",
+//     "headers": {
+//       "x-access-token": "2ffd501228722984feea7ceb9cf73388",
+//       "x-rapidapi-host": "travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com",
+//       "x-rapidapi-key": "d6f870f92fmsh06e8bba4585eef3p12aa21jsn04f58e145da2"
+//     }
+//   }).then(function(response) {
+//     return response.json();
+//   }).then(function(json) {
+//     console.log(json);
+    
+//     /* Grab the Flight Data and Return to the Web Page */
+//     let results = "";
+//     results += '<h2>Flight info from ' + origin +  "to " + destination + "</h2>";
+//     results += "<p>";
+//     results += "TEST TEST TEST TEST TEST";
+//     results += "</p>";
+//     document.getElementById("flight-data").innerHTML = results;
+//   }).catch(err => {s
+//     console.error(err);
+//   });
+// }
+
+document.getElementById("submit").addEventListener("click", function(event) {
+  event.preventDefault();
+  const origin = document.getElementById("origin").value;
+  const destination = document.getElementById("destination").value;
+  const currency = document.getElementById("currency").value;
+
   if (origin === "") {
     alert("Origin must be filled out");
     return false;
@@ -16,15 +64,10 @@ function validateForm() {
     return false;
   }
   else if (origin === destination) {
-    alert("Origin must be different than the destination")
+    alert("Origin must be different than destination")
     return false;
   }
 
-  getCheapestFlight(origin, destination, currency);
-}
-
-/* Function that Fetches the Cheapest Flight Using an External API */
-function getCheapestFlight(origin, destination, currency) {
   fetch("https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/v1/prices/cheap?origin=" + origin + "&page=None&currency=" + currency + "&destination=" + destination, {
     "method": "GET",
     "headers": {
@@ -33,14 +76,22 @@ function getCheapestFlight(origin, destination, currency) {
       "x-rapidapi-key": "d6f870f92fmsh06e8bba4585eef3p12aa21jsn04f58e145da2"
     }
   })
-  .then(response => {
-    console.log("-------------------HERE--------------------");
-    console.log(response);
+  .then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    console.log(json);
+
+    let results = "";
+    results += "<h2>Flight info from " + origin +  " to " + destination + "</h2>";
+    results += "<p>";
+    for (let i = 0; json.data[destination].length; i++) {
+      results += json.data[destination][i];
+      console.log(json.data[destination][i]);
+    }
     
-    /* Add to the Flight Data... */
-    // let output = document.getElementById("flight-data");
-  })
-  .catch(err => {
-    console.error(err);
+    results += "TEST TEST TEST TEST TEST";
+    results += "</p>";
+    console.log(results);
+    document.getElementById("flight-data").innerHTML = results;
   });
-}
+});
